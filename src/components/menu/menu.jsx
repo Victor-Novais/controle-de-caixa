ee8e2500c5cf1d8e2e5de0e2bb53f41137ff81bb
VictorNovais <victornovais1337@gmail.com>
2024-07-23 14:24:53 -0300

    up-menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,24 +1,28 @@
-import "./menu.css"
+import "./menu.css";
+import { FaTachometerAlt, FaRegEdit, FaList, FaShoppingCart } from 'react-icons/fa';
 
 export default function Menu({ setCurrentPage }) {
     return (
         <div className="menu">
             <div className="options">
-                <div onClick={() => setCurrentPage('main')}>
+                <div className="option" onClick={() => setCurrentPage('main')}>
+                    <FaTachometerAlt className="icon" size={20} />
                     <h1>Performance</h1>
                 </div>
-                <div onClick={() => setCurrentPage('register')}>
+                <div className="option" onClick={() => setCurrentPage('register')}>
+                    <FaRegEdit className="icon" size={20} />
                     <h1>Cadastrar</h1>
                 </div>
-                <div onClick={() => setCurrentPage('product-list')}>
+                <div className="option" onClick={() => setCurrentPage('product-list')}>
+                    <FaList className="icon" size={20} />
                     <h1>Lista de Produtos</h1>
                 </div>
-                <div onClick={() => setCurrentPage('purchase')}>
+                <div className="option" onClick={() => setCurrentPage('purchase')}>
+                    <FaShoppingCart className="icon" size={20} />
                     <h1>Efetuar Compra</h1>
                 </div>
             </div>
-
             <div className="line-1"></div>
-        </div >
-    )
+        </div>
+    );
 }
